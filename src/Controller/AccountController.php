56100e025ab91f4185b824b6893fff61ed9a46ac
Simon Escure <simon_escure@yahoo.fr>
2018-10-16 16:28:21 +0200

    Logout system

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,16 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccountController extends AbstractController
 {
     /**
+     * Login route
+     * 
      * @Route("/login", name="account_login")
+     * 
+     * @return Response
      */
     public function login()
     {
         return $this->render('account/login.html.twig');
+    }
+
+    /**
+     * Logout
+     *
+     * @Route("/logout", name="account_logout")
+     * 
+     * @return void
+     */
+    public function logout() {
+
     }
 }
