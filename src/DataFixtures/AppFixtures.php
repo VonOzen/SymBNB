06b422bcd9a3538b65e481fee9059c8677c99e3e
Simon Escure <simon_escure@yahoo.fr>
2018-10-17 14:21:53 +0200

    Adding new Entity Role - fixing getRoles for setting multiple roles - creating table Role & role_user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\Role;
 use App\Entity\User;
 use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -21,6 +22,22 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('en_GB');
+
+        $adminRole = new Role();
+        $adminRole->setTitle('ROLE_ADMIN');
+        $manager->persist($adminRole);
+
+        $adminUser = new User();
+        $adminUser->setFirstName('Simon')
+                  ->setLastName('Escure')
+                  ->setEmail('hello@simonescure.fr')
+                  ->setHash($this->encoder->encodePassword($adminUser, 'password'))
+                  ->setAvatar('https://avatars.io/twitter/HuguesOFraises')
+                  ->setIntroduction($faker->sentence())
+                  ->setDescription('<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>')
+                  ->addUserRole($adminRole);
+
+        $manager->persist($adminUser);
 
         // Adding fake users
         $users = [];
