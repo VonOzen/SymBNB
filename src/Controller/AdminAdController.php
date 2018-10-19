b94448729a96e3c6232ebc5d9c9cd8f5613f1f5f
Simon Escure <simon_escure@yahoo.fr>
2018-10-19 16:02:14 +0200

    Adding edit and remove ad for admin - admin edit form template

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AnnonceType;
 use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,5 +24,67 @@ class AdminAdController extends AbstractController
         return $this->render('admin/ad/index.html.twig', [
             'ads' => $repo->findAll()
         ]);
+    }
+
+    /**
+     * Allow admin to edit ad
+     * 
+     * @Route("/admin/ads/{id}/edit", name="admin_ads_edit")
+     *
+     * @param Ad $ad
+     * @param Request $_REQUEST
+     * @param ObjectManager $manager
+     * @return Response
+     */
+    public function edit(Ad $ad, Request $request, ObjectManager $manager)
+    {
+        $form = $this->createForm(AnnonceType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Changes for \"<strong>{$ad->getTitle()}</strong>\" have been saved !"
+            );
+        }
+
+        return $this->render('admin/ad/edit.html.twig', [
+            'ad' => $ad,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Allow admin to delete an ad
+     * 
+     * @Route("/admin/ads/{id}/delete", name="admin_ads_delete")
+     *
+     * @param Ad $ad
+     * @param ObjectManager $manager
+     * @return Response
+     */
+    public function delete(Ad $ad, ObjectManager $manager)
+    {
+        if(count($ad->getBookings()) > 0) {
+            $this->addFlash(
+                'warning',
+                "You can't delete ad \"<strong>{$ad->getTitle()}</strong>\" because " . count($ad->getBookings()) . " booking(s) are related."
+            );
+        } else {
+
+            $manager->remove($ad);
+            $manager->flush();
+    
+            $this->addFlash(
+                'success',
+                "Ad \"<strong>{$ad->getTitle()}</strong>\" has been removed !"
+            );
+        }
+
+        return $this->redirectToRoute('admin_ads_index');
     }
 }
