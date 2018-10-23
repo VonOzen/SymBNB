f36a60041c4cc2038796fb925a2ce4328d8012b9
Simon Escure <simon_escure@yahoo.fr>
2018-10-23 11:58:21 +0200

    adding dql queries for homepage template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -9,11 +11,17 @@ class HomeController extends Controller
 {
   /**
    * Go to homepage function
+   * 
    * @Route("/", name="homepage")
-   * @return void
+   * 
+   * @param AdRepository $repo
+   * @return Response
    */
-  public function homeAction()
+  public function home(AdRepository $adRepo, UserRepository $userRepo)
   {
-    return $this->render('DefaultPages/home.html.twig');
+    return $this->render('default/home.html.twig', [
+      'ads'   => $adRepo->findBestAds(3),
+      'users' => $userRepo->findBestUsers()
+    ]);
   }
 }
